97363953a83941107ce5b51f27c2cd8385c7b1d6
juan araña <juan.arana@darecode.com>
2022-03-10 18:59:38 +0000

    mensajes a constantes

---
+++ src/Constants/SignWarManagerConstants.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Constants;
+
+class SignWarManagerConstants
+{
+    const ALREADY_WINNER = "The firm is already a winner...";
+    const NEVER_WINNER = "The firm can never be a winner...";
+    const JUDMENT_IS_TIED = "The judgment is tied...";
+    const ONLY_ONE_QUAD = "Se debe indicar un (solo) caracter \"#\" ";
+    const ONLY_ONE_QUAD_SIGN = "Sólo una de las firmas puede contener el caracter \"#\" ";
+    const NO_RIGHT_VALIDATED_SIGN = "Las firmas no se han validado correctamente, mensaje:";
+
+
+}
